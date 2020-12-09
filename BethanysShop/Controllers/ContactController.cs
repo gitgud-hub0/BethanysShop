48d8c6c666d10d880d7cfdffbc9447c79c1e9041
Haobin Qu <hq617@york.ac.uk>
2020-12-09 00:33:46 +0000

    Added custom email tag helper to be used in the newly added Contact Us View

---
+++ BethanysShop/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
