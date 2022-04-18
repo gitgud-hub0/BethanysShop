ff98c2c5437d13b86eb7c6bd19804587055bfd88
Haobin Qu <hq617@york.ac.uk>
2022-04-18 17:29:52 +0100

    tidy up

--- BethanysShop/Startup.cs
+++ BethanysShop/Startup.cs
@@ -35,7 +35,7 @@ namespace BethanysShop
             //service.AddScoped, singleton per request, discarded after each request, good to work incombination with data access
 
             services.AddDbContext<AppDbContext>(options => 
-                options.UseSqlServer(Configuration.GetConnectionString("AwsRdsConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             //added for identities functionality
             services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
